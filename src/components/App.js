b9719c596b2a320b7ea7f6dbc3ece20222116b7b
Danielle Thompson <danithompson74@gmail.com>
2021-03-01 11:12:24 -0800

    Create initial header
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './components/App.css';
+import Header from './Header';
 
 function App() {
   return (
     <>
+      <Header />
       {/* app stuff here */}
     </>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+  return(
+    <>
+      <h1>Choose Your Own Adventure!</h1>
+    </>
+  )
+};
+
+export default Header;
