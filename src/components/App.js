1b8bc9cc7ecb3ba15684b3ff123e82f1f425de61
Danielle Thompson <danithompson74@gmail.com>
2021-03-01 15:08:40 -0800

    Add ternary gameStarted prompts to app
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import Header from './Header';
 import {useSpring, animated} from 'react-spring'; //fetch imports
 
 function App() {
+
   const props = useSpring({opacity: 1, from: {opacity: 0}}); // define your spring
-  return <animated.div style={props}><h1>I will fade in</h1></animated.div> //tie animated values to view
-  // return (
-  //   <>
-  //     <Header />
-  //     {/* app stuff here */}
-  //   </>
-  // );
-}
+
+  return (
+    <> <animated.div style={props}><h1>I will fade in</h1></animated.div>
+      {this.state.gameStarted
+        ? <Screen prompt={this.state.prompt} choiceA={this.state.choiceA} choiceB={this.state.choiceB} choiceC={this.state.choiceC} changePath={this.changePath}/>
+        : <div>
+            <div className="intro"></div>
+            <button onClick={this.goToScreen}>Go to start</button>
+            <p></p>
+          </div>
+      }
+    </>
+    );
+  }
 
 export default App;
