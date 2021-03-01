1fc9e030cc792ae0e534fa7ed71c390c46595972
Danielle Thompson <danithompson74@gmail.com>
2021-03-01 15:37:48 -0800

    Update app without h1
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {useSpring, animated} from 'react-spring'; //fetch imports
+import Screen from './Screen';
 
 function App() {
 
   const props = useSpring({opacity: 1, from: {opacity: 0}}); // define your spring
 
   return (
-    <> <animated.div style={props}><h1>I will fade in</h1></animated.div>
+    <> <animated.div style={props}></animated.div>
       {this.state.gameStarted
         ? <Screen prompt={this.state.prompt} choiceA={this.state.choiceA} choiceB={this.state.choiceB} choiceC={this.state.choiceC} changePath={this.changePath}/>
         : <div>
