03f41a22633935cb61c4477bfe08b87abc8d5299
Danielle Thompson <danithompson74@gmail.com>
2021-03-02 11:18:37 -0800

    Refactor intro and app.js files Co-authored-by: Garrett Brown <gman9mm@live.com> co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import {useSpring, animated} from 'react-spring'; //fetch imports
 import Screen from './Screen';
+import GameControl from './GameControl';
+
 
 function App() {
-
   const props = useSpring({opacity: 1, from: {opacity: 0}}); // define your spring
-
   return (
-    <> <animated.div style={props}></animated.div>
-      {this.state.gameStarted
-        ? <Screen prompt={this.state.prompt} choiceA={this.state.choiceA} choiceB={this.state.choiceB} choiceC={this.state.choiceC} changePath={this.changePath}/>
-        : <div>
-            <div className="intro"></div>
-            <button onClick={this.goToScreen}>Go to start</button>
-            <p></p>
-          </div>
-      }
+    <>
+    {/* <animated.div style={props}></animated.div> */}
+      <GameControl/>
+
     </>
     );
   }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,6 +6,7 @@
 
 import React from 'react';
 import {useSpring, animated} from 'react-spring';
+import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 
 function Intro() {
   return(
@@ -13,6 +14,11 @@ function Intro() {
       <Header />
       <p>This choose-your-own-adventure follows the day in the life of an Epicodus student.</p>
       <Button onClick={goToScreen}></Button>
+      {/* <Parallax pages={3} scrolling={false} horizontal ref={ref => (this.parallax = ref)}>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <span onClick={() => this.parallax.scrollTo(1)}>Layers can contain anything</span>
+        </ParallaxLayer>
+      </Parallax> */}
     </>
   )
 }
