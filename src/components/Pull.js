9ec47cfae8c2353c840b684c53c8a02ba2299d85
Danielle Thompson <danithompson74@gmail.com>
2021-03-03 15:07:10 -0800

    Get pull working after refactor
    Co-authored-by: Patrick Osten <posten.coding@gmail.com>
    Garrett Dean Brown (He/Him)03/01/2021
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- src/components/Pull.js
+++ src/components/Pull.js
@@ -1,16 +1,31 @@
-import React from 'react'
-import clamp from 'lodash-es/clamp'
-import { useSpring, animated } from 'react-spring'
-import { useGesture } from 'react-with-gesture'
-import './styles.css'
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
+import { useDrag } from 'react-use-gesture';
 
 function Pull() {
   const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }))
-  const bind = useGesture(({ down, delta, velocity }) => {
-    velocity = clamp(velocity, 1, 8)
-    set({ xy: down ? delta : [0, 0], config: { mass: velocity, tension: 500 * velocity, friction: 50 } })
+  // Set the drag hook and define component movement based on gesture data
+  const bind = useDrag(({ down, movement }) => {
+    set({ xy: down ? movement : [0, 0] })
   })
-  return <animated.div {...bind()} style={{ transform: xy.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`) }} />
+  // Bind it to a component
+  return (
+    <animated.div
+      {...bind()}
+      style={{
+        width: "100px",
+        height: "100px",
+        background: "hotpink",
+        borderRadius: "60%",
+        cursor: "-webkit-grab",
+        display: "flex",
+        alignItems: "left",
+        justifyContent: "flex-start",
+        whiteSpace: "normal",
+        transform: xy.interpolate((x, y) => `translate3d(${x}px, ${y}px, 0)`)
+      }}
+    />
+  )
 }
 
 export default Pull;
