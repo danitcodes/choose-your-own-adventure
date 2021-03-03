b73c655057ddf519a30a674149a0e7539ba3a501
Danielle Thompson <danithompson74@gmail.com>
2021-03-03 15:33:17 -0800

    Add pug horse to scripts animation
    Co-authored-by: Garrett Brown <gman9mm@live.com>
    Co-authored-by: Patrick Osten <posten.coding@gmail.com>

--- src/components/Script.js
+++ src/components/Script.js
@@ -1,20 +1,21 @@
 import { render } from 'react-dom';
 import React from 'react';
+import Pug from './img/pug-horse.jpg';
+import Shawn from './img/shawn.png'
 import { useSpring, animated } from 'react-spring';
+import './ScriptStyles.css'
 
 export default function Script() {
   const props = useSpring({
-    from: { left: '0%', top: '0%', width: '0%', height: '0%', background: 'lightgreen' },
+    from: { position: 'relative', top: '0%', left: '200px'},
     to: async next => {
       while (1) {
-        await next({ left: '0%', top: '0%', width: '120px', height: '120px', background: 'lightblue' })
-        await next({ height: '100px', background: 'lightgreen' })
-        await next({ width: '100px', left: '50%', background: 'lightgoldenrodyellow' })
-        await next({ top: '0%', height: '80px', background: 'lightpink' })
-        await next({ top: '50%', height: '50px', background: 'lightsalmon' })
-        await next({ width: '10px', background: 'lightslategrey' })
+        await next({  top: '0px' })
+        await next({  top: '40px' })
       }
     },
   })
-  return <animated.div className="script-box" style={props} />
+  return <animated.div className="script-box" style={props} >
+    <img id="pug" src={Pug}></img>
+  </animated.div>
 }
